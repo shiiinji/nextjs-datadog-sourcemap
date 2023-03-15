dae1dd705e8d970fba13ae45b199d5798d0f75cc
shiiinji <shinji.toys@gmail.com>
2023-03-15 21:47:42 +0900

    feat: run multiple upload sourcemap

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,23 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  productionBrowserSourceMaps: true,
+  webpack: (config) => {
+    config.devtool = 'hidden-source-map'
+
+    return config
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*.map',
+          destination: '/404'
+        }
+      ]
+    }
+  }
 };
+
+module.exports = nextConfig;

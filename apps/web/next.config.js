155e318a758d37beecde78e9cca6bb44fbc9d337
shiiinji <shinji.toys@gmail.com>
2023-03-15 16:55:08 +0900

    add: hidden-source-map and rewrite setting

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,23 @@
-const buildMode = process.env.BUILD_MODE
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  productionBrowserSourceMaps: buildMode === 'ci',
+  productionBrowserSourceMaps: true,
+  webpack: (config) => {
+    config.devtool = 'hidden-source-map'
+
+    return config
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*.map',
+          destination: '/404'
+        }
+      ]
+    }
+  }
 };
+
+module.exports = nextConfig;
